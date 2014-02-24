e9ff4ab845dfda26d8458779ba1465127149c756
Federico Bucchi <b.federico@gmail.com>
2014-02-23 19:06:11 -0800

    add connect and open in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,21 +21,50 @@ module.exports = function(grunt) {
         }
       }
     },
+    coffee: {
+      compile: {
+        files: {
+          'www/js/main.js': 'coffee/main.coffee'
+        }
+      }
+    },
     watch: {
-      style: {
-        files: ['less/*'],
+      less: {
+        files: ['less/*.less'],
         tasks: ['less']
       },
-      html: {
-        files: ['haml/*'],
+      haml: {
+        files: ['haml/*.haml'],
         tasks: ['haml']
+      },
+      coffee: {
+        files: ['coffee/*.coffee'],
+        tasks: ['coffee']
+      },
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'www'
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:9001',
+        app: 'Google Chrome'
       }
     },
     copy: {
       all: {
-        files: [
-          { expand: true, cwd: 'lib/', src: '**', dest: 'www/js/', filter: 'isFile' }
-        ]
+        files: [{
+          expand: true,
+          cwd: 'lib/',
+          src: '**',
+          dest: 'www/js/',
+          filter: 'isFile'
+        }],
       }
     }
   });
@@ -44,8 +73,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-haml');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
 
-  grunt.registerTask('dev', ['watch']);
-  grunt.registerTask('move', ['copy']);
+  grunt.registerTask('dev', ['connect', 'open', 'watch']);
+  grunt.registerTask('www', ['copy', 'less', 'haml', 'coffee']);
 
 };
